1f7b5c5be23a1b16cfc7563a3bc17fcd39854b59
zijiedian <wwj1654206898@qq.com>
2025-08-15 16:45:31 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,66 +1,59 @@
 // swift-tools-version:5.9
-import Foundation
 import PackageDescription
 
 let package = Package(
     name: "KSPlayer",
     defaultLocalization: "en",
-    platforms: [.macOS(.v10_15), .macCatalyst(.v13), .iOS(.v13), .tvOS(.v13),
-                .visionOS(.v1)],
+    platforms: [
+        .macOS(.v10_15),
+        .macCatalyst(.v13),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .visionOS(.v1)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // 主播放器库
         .library(
             name: "KSPlayer",
-            // todo clang: warning: using sysroot for 'iPhoneSimulator' but targeting 'MacOSX' [-Wincompatible-sysroot]
-//            type: .dynamic,
             targets: ["KSPlayer"]
         ),
+        // 单独暴露 DisplayCriteria，方便外部直接 import
+        .library(
+            name: "DisplayCriteria",
+            targets: ["DisplayCriteria"]
+        )
+    ],
+    dependencies: [
+        // FFmpegKit 作为静态依赖声明，这样 SPM 会自动拉取
+        .package(url: "https://github.com/kingslay/FFmpegKit.git", from: "6.1.3")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // KSPlayer 主模块
         .target(
             name: "KSPlayer",
             dependencies: [
                 .product(name: "FFmpegKit", package: "FFmpegKit"),
-//                .product(name: "Libass", package: "FFmpegKit"),
-//                .product(name: "Libmpv", package: "FFmpegKit"),
-                "DisplayCriteria",
+                "DisplayCriteria"
             ],
-            resources: [.process("Metal/Shaders.metal")],
+            resources: [
+                .process("Metal/Shaders.metal")
+            ],
             swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency"),
+                // 仅在 Swift 5.9 及以上可用
+                .enableExperimentalFeature("StrictConcurrency")
             ]
         ),
+        // DisplayCriteria 模块
         .target(
             name: "DisplayCriteria"
         ),
+        // 测试模块
         .testTarget(
             name: "KSPlayerTests",
             dependencies: ["KSPlayer"],
-            resources: [.process("Resources")]
-        ),
+            resources: [
+                .process("Resources")
+            ]
+        )
     ]
 )
-
-var ffmpegKitPath = FileManager.default.currentDirectoryPath + "/FFmpegKit"
-if !FileManager.default.fileExists(atPath: ffmpegKitPath) {
-    ffmpegKitPath = FileManager.default.currentDirectoryPath + "/../FFmpegKit"
-}
-
-if !FileManager.default.fileExists(atPath: ffmpegKitPath), let url = URL(string: #file) {
-    let path = url.deletingLastPathComponent().path
-    // 解决用xcode引入spm的时候，依赖关系出错的问题
-    if !path.contains("/checkouts/") {
-        ffmpegKitPath = path + "/../FFmpegKit"
-    }
-}
-
-if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
-    package.dependencies += [
-        .package(path: ffmpegKitPath),
-    ]
-} else {
-    package.dependencies += [
-        .package(url: "https://github.com/kingslay/FFmpegKit.git", from: "6.1.3"),
-    ]
-}
